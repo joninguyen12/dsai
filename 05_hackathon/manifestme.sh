2b34d19a22912d7d8bbcdfb84e00641cb6d8a410
joniqnguyen <jonin@uw.edu>
2026-03-09 04:41:02 -0700

    add manifest back

--- 05_hackathon/manifestme.sh
+++ 05_hackathon/manifestme.sh
@@ -5,6 +5,9 @@
 # for deploying to Posit Connect.
 
 # Install rsconnect package for Python
-pip install rsconnect-python
-# Write a manifest.json file for the Shiny Python app, directing it to the folder
-rsconnect write-manifest shiny 05_hackathon/seattle-congestion-app
+pip install --upgrade rsconnect-python
+
+# Write (or overwrite) a manifest.json file for the Shiny Python app in this folder.
+# We point to the current directory (.) and use app:app as the entrypoint,
+# which matches the Shiny Express app object defined in app.py.
+rsconnect write-manifest shiny . --entrypoint app:app --overwrite
